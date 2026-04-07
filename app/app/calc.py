76dd889796bbc1c324d6bbb9886279c9c0ba65a0
Iván Sánchez <ivan.sanchez@nuvolar.com>
2026-04-07 10:05:35 +0200

    Fix linter

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,6 +7,8 @@ def add(x, y):
     """Add x and y and return result."""
     return x + y
 
+
 def subtract(x, y):
     """Subtract y from x and return result."""
     return x - y
+
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,6 +6,7 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to wait for the database to be available."""
 
@@ -21,3 +22,4 @@ class Command(BaseCommand):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
+
